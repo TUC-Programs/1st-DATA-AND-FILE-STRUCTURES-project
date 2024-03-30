2457e5dfb65edc2bf250eee9ae4cd81ae0fce852
jim <61980746+jimgreekgamerYT@users.noreply.github.com>
2024-03-30 18:11:04 +0200

    Update AAList.java
    Added Insert function into AAList. Not the final form but hopefully it works correctly.

--- Data_Stractures/org/tuc/list/AAList.java
+++ Data_Stractures/org/tuc/list/AAList.java
@@ -1,5 +1,64 @@
 package org.tuc.list;
 
-public class AAList {
+import org.tuc.Element;
+import org.tuc.List;
 
+public class AAList implements List{
+    private static final int MAX_SIZE = 10;
+    protected Node[][] array;
+    protected int tail;
+    protected int next;
+
+    private AAList(){
+        array = new Node[MAX_SIZE][2];
+        tail = -1;
+    }
+
+    public static class  Node {
+        Element element;
+        int key;
+        Node next;
+        Node previous;
+
+        public Node(Element element, int key) {
+            this.element = element;
+            this.key = key;
+            //this.next = next;
+            //this.previous=previous;
+        }
+
+    }
+
+    @Override
+    public boolean insert(Element element){
+        try{
+            if(tail == MAX_SIZE){
+                System.out.println("List is full !");
+                return false;
+            }
+            for (int i = 0; i < MAX_SIZE; i++) {
+                if (array[i][0] == null) {
+                    array[i][0] = new Node(element, element.getKey());
+                    array[i][1] = new Node(null, tail);
+                    break;
+                }
+            }
+            return true;
+        }catch(Exception ex){
+            return false;
+        }
+    }
+
+    @Override
+    public boolean delete(int key){
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
+
+    @Override
+    public Element search(int key){
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'search'");
+    }
+    
 }
