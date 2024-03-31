cc486d17621f516f1c09fba1e940766b803ad7ba
VjonisBoja <149782302+Vboja@users.noreply.github.com>
2024-03-31 12:49:41 +0300

    Update Node.java

--- Data_Stractures/org/tuc/Node.java
+++ Data_Stractures/org/tuc/Node.java
@@ -1,5 +1,14 @@
 package org.tuc;
 
-public interface Node {
-
+public interface Node extends Element {
+   /*
+     * See again if i can do with different way to not trow error in the 
+     * class AList when i write myArray[i].getKey()
+     * 
+     */
+    
+    //This work but i do not know if i can use it
+     int getKey();
+    Object getElement();
+    void setElement(Object element);
 }
