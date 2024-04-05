977975accc4c5405226dccffa5dca77819b1b584
jim <61980746+jimgreekgamerYT@users.noreply.github.com>
2024-04-05 18:39:01 +0300

    Update Node.java

--- Data_Structures/org/tuc/Node.java
+++ Data_Structures/org/tuc/Node.java
@@ -1,24 +1,39 @@
 package org.tuc;
 
-import org.tuc.Element;
 
 public class Node{
 
     public Element element;
     public Node next;
+    public Node tail;
+    public Node head;
     public Node previous;
     int key;
+    
 
     public Node(int key){
         this.key= key;
     }
-    
 
-    public Node(Element element,int key){
-        this.key=key;
-        this.element=element;
+
+    public Element getElement() {
+        return element;
     }
 
+
+    public void setElement(Element element) {
+        this.element = element;
+    }
+
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
 
 
     //Getters and Setters 
