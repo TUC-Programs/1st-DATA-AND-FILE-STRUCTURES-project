0bd6cfd3a08822b6b89f574e91f3b4f5779f8f7a
jim <61980746+jimgreekgamerYT@users.noreply.github.com>
2024-04-11 23:35:32 +0300

    Update ProjectManagerCLI.java

--- Data_Structures/org/tuc/projectManagerFrontEnd/ProjectManagerCLI.java
+++ Data_Structures/org/tuc/projectManagerFrontEnd/ProjectManagerCLI.java
@@ -39,50 +39,67 @@ public class ProjectManagerCLI {
     public static void main(String[] args) {
         TestDataCollector tdc = new TestDataCollector();
         for(int num : N){
-            AAList aaList = new AAList();
+            AAList aaList = new AAList();    // Start the lists
             AList aList = new AList();
             DList dList = new DList();
             SAAList saaList = new SAAList();
             SAList saList = new SAList();
             SDList sdList = new SDList();
 
-            int[] keys = tdc.createRandomNumbers(N[num]);
+            int[] keys = tdc.createRandomNumbers(N[num]);           // Get key for each element and then start the insert proccess on each one
             try {
                 System.out.println("Proccessing AAList ...");
                 tdc.measureRunTime(aaList, keys, 'A');
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-            try {
                 System.out.println("Proccessing AList ...");
                 tdc.measureRunTime(aList, keys, 'A');
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-            try {
                 System.out.println("Proccessing DList ...");
                 tdc.measureRunTime(dList, keys, 'A');
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-            try {
                 System.out.println("Proccessing SAAList ...");
                 tdc.measureRunTime(saaList, keys, 'A');
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-            try {
                 System.out.println("Proccessing SAList ...");
                 tdc.measureRunTime(saList, keys, 'A');
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-            try {
                 System.out.println("Proccessing SDList ...");
                 tdc.measureRunTime(sdList, keys, 'A');
             } catch (Exception e) {
                 System.err.println(e.getMessage());
             }
+
+
+            try {                                                   // Start the delete proccess for each list
+                System.out.println("Proccessing AAList ...");
+                tdc.measureRunTime(aaList, keys, 'B');
+                System.out.println("Proccessing AList ...");
+                tdc.measureRunTime(aList, keys, 'B');
+                System.out.println("Proccessing DList ...");
+                tdc.measureRunTime(dList, keys, 'B');
+                System.out.println("Proccessing SAAList ...");
+                tdc.measureRunTime(saaList, keys, 'B');
+                System.out.println("Proccessing SAList ...");
+                tdc.measureRunTime(saList, keys, 'B');
+                System.out.println("Proccessing SDList ...");
+                tdc.measureRunTime(sdList, keys, 'B');
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+            
+
+            try {                                                   // Start the search proccess for each list
+                System.out.println("Proccessing AAList ...");
+                tdc.measureRunTime(aaList, keys, 'C');
+                System.out.println("Proccessing AList ...");
+                tdc.measureRunTime(aList, keys, 'C');
+                System.out.println("Proccessing DList ...");
+                tdc.measureRunTime(dList, keys, 'C');
+                System.out.println("Proccessing SAAList ...");
+                tdc.measureRunTime(saaList, keys, 'C');
+                System.out.println("Proccessing SAList ...");
+                tdc.measureRunTime(saList, keys, 'C');
+                System.out.println("Proccessing SDList ...");
+                tdc.measureRunTime(sdList, keys, 'C');
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+
         }
 
     }
