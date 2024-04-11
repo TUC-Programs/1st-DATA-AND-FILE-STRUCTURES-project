d2637137b529580c84118fc1e74608d67abbfeed
jim <61980746+jimgreekgamerYT@users.noreply.github.com>
2024-04-11 23:59:23 +0300

    Update ProjectManagerCLI.java

--- Data_Structures/org/tuc/projectManagerFrontEnd/ProjectManagerCLI.java
+++ Data_Structures/org/tuc/projectManagerFrontEnd/ProjectManagerCLI.java
@@ -7,38 +7,13 @@ import org.tuc.list.SAAList;
 import org.tuc.list.SAList;
 import org.tuc.list.SDList;
 import org.tuc.searchtest.TestDataCollector;
+import org.tuc.Globals;
 
 public class ProjectManagerCLI {
 
-    /**
-	 * The maximum generated number used as key for the nodes
-	 */
-	static int MAX_INT_NUMBER = 100000000;
-	
-	/**
-	 * The minimum generated number used as key for the nodes
-	 */	
-	static int MIN_INT_NUMBER = -100000000;
-	
-	/**
-	 * The amount of successful search, i.e. searches for keys that are in the data set
-	 */
-	static int SUCCESS_SEARCHES = 100;
-	
-	
-	/**
-	 * The amount of failure search, i.e. searches for keys that are not in the data set
-	 */
-	static int FAILURE_SEARCHES = 100;
-	
-	/**
-	 * The test is run for different amount of keys in the data set, given by this array 
-	 */
-	static int N[] = { 30, 50, 100, 200, 500, 1000, 5000, 10000, 100000};
-
     public static void main(String[] args) {
         TestDataCollector tdc = new TestDataCollector();
-        for(int num : N){
+        for(int num : Globals.N){
             AAList aaList = new AAList();    // Start the lists
             AList aList = new AList();
             DList dList = new DList();
@@ -46,7 +21,7 @@ public class ProjectManagerCLI {
             SAList saList = new SAList();
             SDList sdList = new SDList();
 
-            int[] keys = tdc.createRandomNumbers(N[num]);           // Get key for each element and then start the insert proccess on each one
+            int[] keys = tdc.createRandomNumbers(num);           // Get key for each element and then start the insert proccess on each one
             try {
                 System.out.println("Proccessing AAList ...");
                 tdc.measureRunTime(aaList, keys, 'A');
